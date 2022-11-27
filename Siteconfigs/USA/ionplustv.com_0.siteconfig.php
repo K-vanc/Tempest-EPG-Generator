5e8b976c2545bed3a19023a1de67e13759fb405d
Kivanc <97025515+K-vanc@users.noreply.github.com>
2022-11-27 15:25:44 +0300

    Add files via upload

---
+++ Siteconfigs/USA/ionplustv.com_0.siteconfig.php
@@ -0,0 +1,34 @@
+<?php 
+/*     Tempest EPG Generator (made by Kvanc)
+https://github.com/K-vanc/Tempest-EPG-Generator.git  */
+return array (
+  'filename' => 'ionplustv.com',
+  'creator_name' => 'Kivanc',
+  'creation_date' => '2022-11-27',
+  'rev_no' => 'R0',
+  'timezone' => 'America/New_York',
+  'culture' => 'en',
+  'max_day' => '14',
+  'rating_system' => 'TVPG',
+  'episodeOption' => '1',
+  'url1' => 'https://ionplustv.com/schedule/##urldate1##',
+  'requestOption1' => '1',
+  'accept_header1' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'content_type1' => 'text/html; charset=UTF-8',
+  'urldate_format1' => 'Y-m-d',
+  'show' => '|#|[-]|#|(<script type="application\\/ld\\+json">\\s*{\\s*"@context": "http:\\\\\\/\\\\\\/schema\\.org",\\s*"@type": "Event",.*?<\\/script>)|(<div class="panel-heading".*?<br clear="all"\\/>)',
+  'start' => '"startDate": "(.*?)\\-\\d+:\\d+",',
+  'start_format' => 'Y-m-d\\TH#i#s',
+  'title' => '"name": "(.*?)",',
+  'subtitle' => 'addthis:title=".*?\\sEP\\s\\d+:\\s(.*?)"',
+  'desc' => '"description": "(.*?)",',
+  'showicon' => '<img.*?src="(.*?)"',
+  'season' => '\\/season\\/(\\d+)',
+  'episode' => '\\/episode\\/(\\d+)',
+  'channel_logo' => '||#add#https://ionplustv.com/static/global/IONPlus-logo1.svg',
+  'rating' => '<p class="tv-rating">(.*?)<p>||#replace#(\\(.*?\\))||',
+  'ccrequestOption1' => '1',
+  'ccchannel_id' => '||#set#ion_plus',
+  'ccchannel_name' => '||#set#ION Plus',
+);
+?>
---
+++ Siteconfigs/USA/iontelevision.com_0.siteconfig.php
@@ -0,0 +1,36 @@
+<?php 
+/*     Tempest EPG Generator (made by Kvanc)
+https://github.com/K-vanc/Tempest-EPG-Generator.git  */
+return array (
+  'filename' => 'iontelevision.com',
+  'creator_name' => 'Kivanc',
+  'creation_date' => '2022-11-27',
+  'rev_no' => 'R0',
+  'timezone' => 'America/New_York',
+  'culture' => 'en',
+  'max_day' => '14',
+  'rating_system' => 'TVPG',
+  'episodeOption' => '1',
+  'url1' => 'https://iontelevision.com/api/v1/programs?date=##urldate1##',
+  'requestOption1' => '1',
+  'accept_header1' => 'application/json, text/plain, */*',
+  'content_type1' => 'application/json',
+  'XMLHttpRequest1' => 'on',
+  'urldate_format1' => 'Y-m-d',
+  'show' => '({"name".*?}})',
+  'start' => '"easternHour":"(\\d+)|#|[| |]"meridiem":"(.*?)"',
+  'start_format' => 'g A',
+  'duration' => '"duration":(\\d+)||#format#s',
+  'title' => '{"name":"(.*?)",',
+  'subtitle' => '"episodeNumber":"(?!0)\\d+","name":"(.*?)"',
+  'desc' => '"description":"(.*?)",|#|[|\\n|]"episodeNumber":"(?!0)\\d+","name":".*?","description":"(.*?)",',
+  'showicon' => '"image":{"url":"(.*?)"',
+  'episode' => '"episodeNumber":"(?!0)(\\d+)',
+  'channel_logo' => '||#add#https://iontelevision.com/images/ion-tv-nav-logo.png',
+  'rating' => '"rating":"(.*?)"',
+  'new' => '"isNew":(true),',
+  'ccrequestOption1' => '1',
+  'ccchannel_id' => '||#set#ion-tv',
+  'ccchannel_name' => '||#set#ION TV',
+);
+?>
