41f630db918da6297106fa68953f8a3a5df7dd34
Kivanc <97025515+K-vanc@users.noreply.github.com>
2023-04-01 08:21:43 +0300

    Add files via upload

---
+++ Siteconfigs/Israel/13tv.co.il_0.siteconfig.php
@@ -0,0 +1,28 @@
+<?php 
+/*     Tempest EPG Generator (made by Kvanc)
+https://github.com/K-vanc/Tempest-EPG-Generator.git  */
+return array (
+  'filename' => '13tv.co.il',
+  'creator_name' => 'Kivanc',
+  'creation_date' => '2023-04-01',
+  'rev_no' => 'R0',
+  'timezone' => 'Asia/Jerusalem',
+  'culture' => 'he',
+  'max_day' => '7.1',
+  'url1' => 'https://13tv.co.il/tv-guide/',
+  'requestOption1' => '1',
+  'accept_header1' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  'cookie_header1' => 'Cookie: SERVERAID=s02-nodejs',
+  'urldate_format1' => 'Y-m-d||#date#-1day',
+  'show' => '({"desc".*?})||#exclude#"show_date":"##urldate1##"',
+  'start' => '"start_time":"(.*?)"',
+  'start_format' => 'H#i',
+  'title' => '"title":"(.*?)",',
+  'desc' => '"desc":"(.*?)",',
+  'showicon' => '"image":"(.*?)"',
+  'channel_logo' => '||#add#https://media.reshet.tv/image/upload/f_auto,q_auto:best/v1653491545/assets/2022/Logo_Beta.png',
+  'ccrequestOption1' => '1',
+  'ccchannel_id' => '||#set#13tv',
+  'ccchannel_name' => '||#set#13TV',
+);
+?>
---
+++ Siteconfigs/Israel/mako.co.il_0.siteconfig.php
@@ -0,0 +1,31 @@
+<?php 
+/*     Tempest EPG Generator (made by Kvanc)
+https://github.com/K-vanc/Tempest-EPG-Generator.git  */
+return array (
+  'filename' => 'mako.co.il',
+  'creator_name' => 'Kivanc',
+  'creation_date' => '2023-04-01',
+  'rev_no' => 'R0',
+  'timezone' => 'UTC',
+  'culture' => 'he',
+  'max_day' => '9.1',
+  'episodeOption' => '1',
+  'url1' => 'https://www.mako.co.il/AjaxPage?jspName=EPGResponse.jsp',
+  'requestOption1' => '1',
+  'accept_header1' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  'host_header1' => 'www.mako.co.il',
+  'show' => '({"DisplayEndTime".*?})',
+  'start' => '"StartTimeUTC":(\\d+)',
+  'start_format' => 'java',
+  'title' => '"ProgramName":"(.*?)",',
+  'desc' => '"EventDescription":"(.*?)",',
+  'category' => '"(Live)Broadcast":true',
+  'showicon' => '"Picture":"(.*?)"',
+  'season' => '"Season":"(.*?)"',
+  'episode' => 'EP (\\d+)"',
+  'channel_logo' => '||#add#https://img.mako.co.il/2023/03/08/LOGOBIGMAKONEW.png',
+  'ccrequestOption1' => '1',
+  'ccchannel_id' => '||#set#mako',
+  'ccchannel_name' => '||#set#Mako',
+);
+?>
