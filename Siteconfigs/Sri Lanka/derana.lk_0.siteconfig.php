b6d6d44e9565afe8da35534f845bb5f0c923ac2e
Kivanc <97025515+K-vanc@users.noreply.github.com>
2024-08-10 11:29:46 +0300

    Add files via upload

---
+++ Siteconfigs/Sri Lanka/derana.lk_0.siteconfig.php
@@ -0,0 +1,30 @@
+<?php 
+/*     Tempest EPG Generator (made by Kvanc)
+https://github.com/K-vanc/Tempest-EPG-Generator.git  */
+return array (
+  'filename' => 'derana.lk',
+  'creator_name' => 'Kivanc',
+  'creation_date' => '2024-08-10',
+  'rev_no' => 'R0',
+  'timezone' => 'Asia/Colombo',
+  'culture' => 'si',
+  'max_day' => '8',
+  'url1' => 'https://derana.lk/api/schedules/##urldate1##||#edit#("time":")(\\d:)##("time":")(.*?)\\s([AP]M)##(AM12)##("date":")(\\d{2})-(\\d{2})-(\\d{4})##("time":")AM##("time":")PM(12)||${1}0${2}##\\1\\3\\2##00##\\1\\4\\3\\2##\\1##\\1\\2',
+  'requestOption1' => '1',
+  'accept_header1' => '*/*',
+  'origin_header1' => 'http://www.derana.lk',
+  'urldate_format1' => 'd-m-Y',
+  'show' => '"all_schedules":\\[.*?(?:{)(.*?)(?:}).*?\\]||#calc#"time":"PM(\\d+)||+12||#replace#("time":")PM||\\1',
+  'sort' => '"date":"(.*?)"|#|[| |]"time":"(.*?)"||#replace#(\\s|:)||',
+  'sortflag' => '2',
+  'sortorder' => '1',
+  'start' => '"time":"(.*?)"||#replace#(\\s|PM)||',
+  'start_format' => 'H#i',
+  'title' => '"dramaName":"(.*?)",',
+  'showicon' => '"imageUrl":"(.*?)"',
+  'channel_logo' => '||#add#http://www.derana.lk/images/logo/logo-color-bg-transparent.webp',
+  'ccrequestOption1' => '1',
+  'ccchannel_id' => '||#set#tv_derana',
+  'ccchannel_name' => '||#set#TV Derana',
+);
+?>
---
+++ Siteconfigs/Sri Lanka/itn.lk_0.siteconfig.php
@@ -0,0 +1,29 @@
+<?php 
+/*     Tempest EPG Generator (made by Kvanc)
+https://github.com/K-vanc/Tempest-EPG-Generator.git  */
+return array (
+  'filename' => 'itn.lk',
+  'creator_name' => 'Kivanc',
+  'creation_date' => '2024-08-10',
+  'rev_no' => 'R0',
+  'timezone' => 'UTC',
+  'culture' => 'si',
+  'max_day' => '7.1',
+  'url1' => 'https://itn.lk/wp-admin/admin-ajax.php?action=wcs_get_events_json&content%5Bwcs_type%5D%5B%5D=##channel##&start=##urldate1##&end=##stopdate1##',
+  'requestOption1' => '1',
+  'accept_header1' => 'application/json, text/plain, */*',
+  'XMLHttpRequest1' => 'on',
+  'urldate_format1' => 'Y-m-d',
+  'stopdate_format1' => 'Y-m-d',
+  'show' => '({"title".*?"meta":.*?})',
+  'start' => '"start":"(.*?)\\+',
+  'start_format' => 'Y-m-d\\TH#i#s',
+  'stop' => '"end":"(.*?)\\+',
+  'stop_format' => 'Y-m-d\\TH#i#s',
+  'title' => '"title":"(.*?)",',
+  'channel_logo' => '||#add#https://itn.lk/wp-content/uploads/2022/03/ITN-Logo.svg',
+  'ccrequestOption1' => '1',
+  'ccchannel_id' => '||#set#15|16',
+  'ccchannel_name' => '||#set#ITN General|ITN Special',
+);
+?>
